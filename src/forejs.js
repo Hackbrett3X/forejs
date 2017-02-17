ed3cbbee46cfac196305572b454f7ae0bbfa56a8
Hackbrett <hollaender.lukas@googlemail.com>
2017-02-17 14:33:15 +0100

    implementation of dependent execution

--- src/forejs.js
+++ src/forejs.js
@@ -1,6 +1,219 @@
 module.exports = fore;
 
-function fore() {
+/**
+ * @param {...*} functions
+ */
+function fore(functions) {
+  if (typeof functions === "object") {
+    var graph = {};
+    var roots = [];
+    Object.getOwnPropertyNames(functions).forEach(function (name) {
+      var fn = functions[name];
 
+      getOrCreateNode(graph, name).function = fn;
+
+      var numberOfInjections = 0;
+
+      var injector = fn.$injector;
+      if (injector instanceof Injector) {
+        injector.injections.forEach(function (injection) {
+          if (injection instanceof Injection) {
+            if (connect(graph, injection.id, name)) {
+              numberOfInjections++;
+            }
+          }
+        });
+
+        var thisInjection = injector.thisInjection;
+        if (thisInjection && thisInjection instanceof Injection) {
+          if (connect(graph, thisInjection.id, name)) {
+            numberOfInjections++;
+          }
+        }
+      }
+
+      if (numberOfInjections === 0) {
+        roots.push(getOrCreateNode(graph, name));
+      }
+
+    });
+
+    var results = {};
+    roots.forEach(function (node) {
+      node.execute(results);
+    });
+
+
+  } else {
+    // TODO: simple case: chain all function calls
+    for (var i = 0; i < arguments.length; i++) {
+      var fn = arguments[i];
+
+
+    }
+  }
 }
 
+/**
+ *
+ * @param {Object.<String, ExecutorNode>} graph
+ * @param {String} from
+ * @param {String} to
+ * @return {boolean} false iff this dependency already existed
+ */
+function connect(graph, from, to) {
+  var fromNode = getOrCreateNode(graph, from);
+  var toNode = getOrCreateNode(graph, to);
+
+  if (fromNode.dependents.indexOf(toNode) < 0) {
+    fromNode.dependents.push(toNode);
+    toNode.dependencies.push(fromNode);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/**
+ * @param {Object.<String, ExecutorNode>} graph
+ * @param {String} id
+ * @returns {ExecutorNode}
+ */
+function getOrCreateNode(graph, id) {
+  return graph[id] || (graph[id] = new ExecutorNode(id));
+}
+
+/**
+ * @typedef {Object.<String, *>} Results
+ */
+
+/**
+ * @param {String} id
+ * @constructor
+ */
+function ExecutorNode(id) {
+  this.id = id;
+  this.dependencies = [];
+  this.executedDependencies = 0;
+  this.dependents = [];
+  this.function = null;
+}
+
+/**
+ * @param {Results} results
+ */
+ExecutorNode.prototype.execute = function execute(results) {
+  var fn = this.function;
+
+  if (fn.$injector instanceof Injector) {
+    fn.$injector.results = results;
+  }
+
+  fn(function (err, res) {
+    if (err != null) {
+      // TODO: error case
+    } else {
+      results[this.id] = res;
+      this.dependents.forEach(function (node) {
+        node.notify(results);
+      });
+    }
+  }.bind(this))
+};
+
+/**
+ * @param {Results} results
+ */
+ExecutorNode.prototype.notify = function (results) {
+  this.executedDependencies++;
+
+  if (this.executedDependencies === this.dependencies.length) {
+    this.execute(results);
+  }
+};
+
+/**
+ * @param {String} id
+ */
+fore.get = function get(id) {
+  return new Injection(id);
+};
+
+/**
+ * @param {String} id
+ * @constructor
+ */
+function Injection(id) {
+  this.id = id;
+}
+
+/**
+ * @param {Results} results
+ * @return {*}
+ */
+Injection.prototype.resolve = function resolve(results) {
+  return results[this.id];
+};
+
+/**
+ * @constructor
+ */
+function Injector() {
+  this.injections = null;
+  this.thisInjection = null;
+  this.results = null;
+}
+
+/**
+ * @callback InjectorFunction
+ * @property {function(...*): InjectorFunction} args
+ * @property {function(Object): InjectorFunction} this
+ */
+
+/**
+ * @return {InjectorFunction}
+ */
+function inject() {
+  var originalFunction = this;
+
+  var injector = new Injector();
+
+  function fn(callback) {
+    var thisInjection = injector.thisInjection;
+    var thisArg = thisInjection && (thisInjection instanceof Injection ?
+            thisInjection.resolve(injector.results) : thisInjection);
+
+    var injections = injector.injections;
+    var args = new Array(injections.length + 1);
+    injections.forEach(function (arg, i) {
+      args[i] = arg instanceof Injection ? arg.resolve(injector.results) : arg;
+    });
+    args[args.length - 1] = callback;
+
+    originalFunction.apply(thisArg, args);
+  }
+
+  fn.args = function args() {
+    var injections = injector.injections = new Array(arguments.length);
+    for (var i = 0; i < arguments.length; i++) {
+      injections[i] = arguments[i];
+    }
+    return fn;
+  };
+
+  fn.this = function ths(object) {
+    injector.thisInjection = object;
+    return fn;
+  };
+
+  fn.$injector = injector;
+
+  return fn;
+}
+
+// add inject to Function prototype
+Object.defineProperty(Function.prototype, "inject", {
+  get: inject,
+  configurable: false,
+  enumerable: false
+});
--- test/forejs-test.js
+++ test/forejs-test.js
@@ -1,9 +1,10 @@
 var expect = require("chai").expect;
 var it = require("mocha").it;
-var before = require("mocha").before;
 var describe = require("mocha").describe;
 
 var fore = require("../src/forejs");
+
+// TODO: duplicate refs, inject.this
 
 function one(callback) {
   setTimeout(function () {
@@ -31,64 +32,101 @@ function error(error, callback) {
 
 describe("General functionality", function () {
 
-  describe("Simple chaining/defer", function () {
-    it("one - defer with 0 arguments", function () {
+  describe("Simple chaining/inject", function () {
+    it("one - inject with 0 arguments", function (done) {
       fore(
-          one.defer(),
+          one,
           function (res) {
             expect(res).to.equal(1);
+            done();
           }
       )
     });
 
-    it("plusOne - defer with 1 argument", function () {
+    it("plusOne - inject with 1 argument", function (done) {
       fore(
-          plusOne.defer(0),
+          plusOne.inject.args(0),
           function (res) {
             expect(res).to.equal(1);
+            done();
           }
       )
     });
 
-    it("plus - defer with 2 arguments", function () {
+    it("plus - inject with 2 arguments", function (done) {
       fore(
-          plus.defer(0, 1),
+          plus.inject.args(0, 1),
           function (res) {
             expect(res).to.equal(1);
+            done();
           }
       )
     });
 
-    it("one plusOne plus 1 - chain them all", function () {
+    it("one plusOne plus 1 - chain them all", function (done) {
       fore(
-          one.defer(),
-          plusOne.defer(),
-          plus.defer(1),
+          one,
+          plusOne,
+          plus.inject.args(1),
           function (res) {
             expect(res).to.equal(3);
+            done();
           }
       )
     });
   });
 
-  describe("Catching errors", function () {
+  describe("Catching errors", function (done) {
     it("catch", function () {
       fore(
-          error.defer("msg").catch(function (error) {
+          error.inject.args("msg").catch(function (error) {
             expect(error).to.equal("msg");
+            done();
           })
       );
     });
 
     it("catch - stop propagation on error", function () {
       fore(
-          error.defer("msg").catch(function (error) {
+          error.inject.args("msg").catch(function (error) {
             expect(error).to.equal("msg");
+            done();
           }),
           function () {
             expect.fail();
+            done();
           }
       );
     });
-  })
+  });
+
+  describe("Complex dependencies", function () {
+    it("plus(one, plusOne(1))", function (done) {
+      fore({
+        one: one,
+        onePlusOne: plusOne.inject.args(1),
+        plus: plus.inject.args(fore.get("one"), fore.get("onePlusOne")),
+        _: (function (plus) {
+          expect(plus).to.equal(3);
+          done();
+        }).inject.args(fore.get("plus"))
+
+        // maybe some sugar:
+        // _: ["plus", function () {
+        //   expect(fore.get("plus").to.equal(3);
+        // }]
+      })
+    });
+
+    it("plus(plusOne(1), plusOne(1)) - duplicate dependency", function (done) {
+      fore({
+        onePlusOne: plusOne.inject.args(1),
+        plus: plus.inject.args(fore.get("onePlusOne"), fore.get("onePlusOne")),
+        _: (function (plus) {
+          expect(plus).to.equal(4);
+          done();
+        }).inject.args(fore.get("plus"))
+      })
+    });
+  });
 });
