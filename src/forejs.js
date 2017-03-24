264c0c4afeafc533b87f1d5a4c32b378c4e5676e
Hackbrett <hollaender.lukas@googlemail.com>
2017-03-24 23:58:03 +0100

    documentation: fore, fore.try, fore.each, fore.collect, fore.reduce

--- src/forejs.js
+++ src/forejs.js
@@ -81,7 +81,52 @@ function id(arg) {
  */
 
 /**
- * @param {...*} functions
+ * The main entry point. Supports two modes:
+ * <ul>
+ *   <li>chain: In chain mode, <code>fore</code> accepts a list of functions that are executed sequentially.</li>
+ *   <li>auto: In auto mode, <code>fore</code> accepts an object where the keys are identifiers and the values are
+ *       functions. The identifiers can be referenced by other functions to retrieve its "return value" (see
+ *       {@link inject} and {@link fore.ref}). ForeJs now figures out the perfect execution order running as many code
+ *       in parallel as possible.</li>
+ * </ul>
+ *
+ * The functions passed may have one of the following forms:
+ * <ul>
+ *   <li>Node-style asynchronous function: Accepts a number of arbitrary arguments followed by an error-first callback
+ *       function. The function must call this callback with either a non-null first argument to signal an error or
+ *       with null as first argument followed by any number of "return values".
+ *       In chain mode those "return values" are directly passed to the next function.
+ *       In auto mode those values are passed to all dependent functions. If more than one value is passed to the
+ *       callback, those values are passed as array to the dependents. Additionally all arguments but the last
+ *       (callback) must be eliminated by injections ({@link inject}).</li>
+ *   <li>Synchronous function: Sometimes you want to mix synchronous functions into asynchronous code. This is perfectly
+ *       ok: Simply put a plain <code>return</code> statement and ignore the callback. If you want to return
+ *       <code>undefined</code> on purpose you will need to invoke the callback, however.</li>
+ *   <li>Promise: Promises are supported, as well. Plain promises must be in a root position.</li>
+ *   <li>Promise returning function: A function may as well return a promise instead of invoking the callback.
+ *       Unfortunately the function will still get passed a callback as last argument. If your function cannot cope
+ *       with the extra argument, simply wrap it with another function.</li>
+ *   <li>Array: As shown in {@link Injector.prototype.args} injections can be syntactically sugared. If you are looking
+ *       for a way to iterate over arrays see {@link fore.each}.</li>
+ *   <li>Instances of {@link Injector}: An Injector is simply the wrapping type of injected functions.</li>
+ * </ul>
+ *
+ * @example
+ * // chain mode:
+ * fore(
+ *     f1,
+ *     f2,
+ *     ...
+ * );
+ * // auto mode:
+ * fore({
+ *   a: fa,
+ *   b: fb,
+ *   c: ["a", "b", fc] // the results of fa and fb are injected into fc once they are ready
+ * });
+ * @param {(Object.<String, function|Array|Injector|Promise>)} functions For auto mode: an object hash with ids as keys and
+ * functions as values.
+ * @param {...function|Array|Injector|Promise} arguments For chain mode: a list of functions.
  */
 function fore(functions) {
   if (typeof functions === "object" && Object.getPrototypeOf(functions) === Object.prototype) {
@@ -230,6 +275,24 @@ function simpleChain(functions) {
   rootInjector.execute(true, 1);
 }
 
+/**
+ * Registers a general error handler.
+ * @callback catch
+ * @param {function(*)} errorHandler A function which accepts one argument - the error.
+ */
+
+/**
+ * Wraps {@link fore} with a try-catch mechanism, which registers a general error handler for all provided functions.
+ * Once any of these functions "returns" an error this error handler will be invoked and the propagation of the
+ * respective execution branch will stopped.
+ *
+ * This error handler can be shadowed for single functions using {@link Injector.prototype.catch}.
+ * @example
+ * fore.try(
+ *     // functions...
+ * ).catch(error => ...)
+ * @return {{catch: catch}} An object with a single function <code>catch</code> to pass the error handler to.
+ */
 fore.try = function () {
   var args = arguments;
   return {
@@ -250,8 +313,28 @@ fore.try = function () {
 };
 
 /**
- * @param {Array|Iterator|Iterable|function} iterable
- * @return {Injector}
+ * Successively returns all values the iterable provides. Values will be processed in parallel.
+ * @see fore.collect
+ * @see fore.reduce
+ * @param {Array|Iterator|Iterable|function|Injector} iterable One of the following:
+ *  <ul>
+ *   <li>An array of arbitrary values. Use to run a sequence of functions for multiple values.</li>
+ *   <li>An Iterator, i.e. an object providing a <code>next</code> function which itself returns objects in the shape of
+ *       <code>{value: value&#124;undefined, done: true&#124;false&#124;undefined}</code>.</li>
+ *   <li>An Iterable, i.e. an object providing a <code>Symbol.iterator</code> to retrieve an Iterator like above.</li>
+ *   <li>A generator function: A function which returns an Iterator (like ES6 <code>function*</code>s do}. If this function
+ *       takes arguments make sure to take care of the respective injections.</li>
+ *  </ul>
+ *  The cases 1 - 3 require <code>fore.each</code> to be in a root position (they don't depend on other functions).
+ *  The last case is allowed in any position.
+ *
+ *  Any iterable type may also provide promises as values in order to asynchronously generate values.
+ *
+ *  Values generated by <code>fore.each</code> will be propagated through subsequent functions causing them to be called
+ *  multiple times. If a function has several dependencies (in auto mode) that originate in a <code>fore.each</code>,
+ *  it will be invoked with any possible combinations of the incoming values.
+ * @return {Injector} An injector which can be used to inject arguments to the iterable in case it is a generator
+ * function (<code>function*</code>).
  */
 fore.each = function foreEach(iterable) {
   var injector = (iterable instanceof Injector) ? iterable : new Injector(iterable);
@@ -260,8 +343,14 @@ fore.each = function foreEach(iterable) {
 };
 
 /**
+ * The counterpart to {@link fore.each}. Collects all values that were generated by {@link fore.each} and modified by
+ * in-between functions. The results will be passed to <code>fn</code> as array. If it depends on multiple iterables
+ * <code>fore.collect</code> waits for all branches to finish and each result array will passed as separate argument.
+ *
+ * Naturally for asynchronous code the result array will not necessarily have the same order as the input.
+ * @see fore.reduce
  * @param {function|Injector|Array.<*>} fn
- * @return {Injector}
+ * @return {Injector} An injector which can be used to inject arguments to the function.
  */
 fore.collect = function (fn) {
   var injector = desugar(fn);
@@ -270,9 +359,28 @@ fore.collect = function (fn) {
 };
 
 /**
- * @param {function|Injector|Array.<*>} fn
- * @param {*} initialValue
- * @return Injector
+ * Another counterpart to {@link fore.each}. Behaves much like {@link fore.collect} but provides the results not as array
+ * but in a fashion similar to {@link Array.prototype.reduce}: <code>fn</code> will be called once for each element of
+ * the result. It will get passed the accumulator followed by dependency result(s) followed by a callback. The
+ * "return value" of this call will be the new accumulator for the next invocation. For the first invocation
+ * the accumulation variable is <code>initialValue</code>.
+ *
+ * If there are more than one dependencies (and several of these originate in a {@link fore.each}) <code>fn</code> will
+ * be called once for every possible combination of the incoming values.
+ *
+ * Likewise, no specific execution order can be guaranteed.
+ * @example
+ * fore(
+ *     fore.each([1, 2, 3),
+ *     plusOne,
+ *     fore.reduce((accumulation, value, callback) => callback(null, accumulation * value), 1),
+ *     console.log
+ *     // result: 1 * 2 * 3 * 4 = 24
+ * )
+ * @param {function|Injector|Array.<*>} fn The function which will be invoked with
+ *   <code>(accumulator, value, ..., callback)</code>
+ * @param {*} initialValue The value for the accumulator during the first invocation.
+ * @return {Injector} An injector which can be used to inject arguments to the function.
  */
 fore.reduce = function (fn, initialValue) {
   var injector = desugar(fn);
